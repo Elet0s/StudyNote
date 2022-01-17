7dd4f5931ad2be5999ef4e5594981ee661bf1e59
Eletos <lthlovelyg@naver.com>
2022-01-17 23:48:21 +0900

    git test

--- Project004/달력.cpp
+++ Project004/달력.cpp
@@ -1,6 +1,13 @@
-﻿#include <iostream>
-
-int main()
+﻿int MyPrintf_s(const char* const _Text)
 {
-  
+	int Count = 0;
+
+	while (0 != _Text[Count])
+	{
+		putchar(_Text[Count]);
+		Count += 1;
+	}
+
+	return Count;
 }
+//git 변경점
